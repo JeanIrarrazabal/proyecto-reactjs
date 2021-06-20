3c780a76b000bf71fad0940bf374bfbff7497e47
JeanIrarrazabal <jp_532689@hotmail.com>
2021-06-20 18:30:30 -0400

    NavBar
    Se agrega NavBar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import {NavbarComponent} from './Components/NavBarComponenet/index.jsx';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+// información que muesta en el HTML
+function App() {
+  return (
+      <NavbarComponent/>
+  );
+}
+
+export default App;
+
